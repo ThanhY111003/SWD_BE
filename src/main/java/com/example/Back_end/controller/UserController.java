7c912ac07d084afadb3e0cbfa6921bb3f9cb3af0
hai65 <vyastaroth123@gmail.com>
2025-10-02 22:00:26 +0700

    Mô tả ngắn gọn thay đổi

--- src/main/java/com/example/Back_end/controller/UserController.java
+++ src/main/java/com/example/Back_end/controller/UserController.java
@@ -2,12 +2,14 @@ package com.example.Back_end.controller;
 
 import com.example.Back_end.entity.User;
 import com.example.Back_end.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User Controller", description = "Quản lý người dùng")
 public class UserController {
     private final UserService userService;
 
