595598a0bf80aff66ba12aa94942871e08aa376e
hai65 <vyastaroth123@gmail.com>
2025-11-13 21:00:11 +0700

    13/11/2025 ver 2

--- src/main/java/com/example/Back_end/repository/NotificationRepository.java
+++ src/main/java/com/example/Back_end/repository/NotificationRepository.java
@@ -4,6 +4,11 @@ import com.example.Back_end.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByMember_MemberId(Long memberId);
+    List<Notification> findByStaff_StaffId(Long staffId);
+    List<Notification> findBySupporter_SupporterId(Long supporterId);
 }
